4c5ad87844693c9885b17a74396b0ee04cde918c
thinkofher <piaskupiasku@gmail.com>
2020-01-15 15:04:21 +0100

    init commit

---
+++ app/go.mod
@@ -0,0 +1,17 @@
+module github.com/thinkofher/pants/app
+
+go 1.13
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.11 // indirect
+	github.com/ozgio/strutil v0.3.0
+	github.com/valyala/fasttemplate v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20200115085410-6d4e4cb37c7d // indirect
+	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
+	golang.org/x/sys v0.0.0-20200113162924-86b910548bc1 // indirect
+	golang.org/x/text v0.3.2 // indirect
+)
---
+++ app/main.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/ozgio/strutil"
+)
+
+const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const KeyLength = 7
+
+var keyNoValueErr = errors.New("db: there is no value assigned to given key")
+var keyTakenErr = errors.New("db: key is already taken")
+
+func RandomString(length int) (string, error) {
+	return strutil.Random(chars, length)
+}
+
+type Entry struct {
+	Key string `json:"key"`
+	URL string `json:"url"`
+}
+
+func NewEntry(URL string) (*Entry, error) {
+	key, err := RandomString(KeyLength)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Entry{Key: key, URL: URL}, nil
+}
+
+type DB map[string]string
+
+func (db DB) SaveEntry(e Entry) error {
+	_, ok := db[e.Key]
+	if ok {
+		return keyTakenErr
+	}
+
+	db[e.Key] = e.URL
+	return nil
+}
+
+func (db DB) GetURL(key string) (string, error) {
+	_, ok := db[key]
+	if !ok {
+		return "", keyNoValueErr
+	}
+
+	return db[key], nil
+}
+
+func (db DB) GetEntry(key string) (*Entry, error) {
+	value, err := db.GetURL(key)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Entry{Key: key, URL: value}, nil
+}
+
+type URL struct {
+	Value string `json:"value"`
+}
+
+func ShortURL(db *DB) echo.HandlerFunc {
+
+	var bindErr = echo.NewHTTPError(http.StatusBadRequest,
+		"Failed to parse given data.")
+	var shortErr = echo.NewHTTPError(http.StatusInternalServerError,
+		"Could not short given url.")
+	var luckyErr = echo.NewHTTPError(http.StatusInternalServerError,
+		"Could not short given url. Please try again.")
+
+	return func(c echo.Context) error {
+
+		url := &URL{Value: ""}
+		if err := c.Bind(url); err != nil {
+			return bindErr
+		}
+
+		entry, err := NewEntry(url.Value)
+		if err != nil {
+			return shortErr
+		}
+
+		err = db.SaveEntry(*entry)
+		if err == keyTakenErr {
+			return luckyErr
+		} else if err != nil {
+			return shortErr
+		}
+
+		return c.JSON(http.StatusCreated, entry)
+	}
+}
+
+func GetShort(db *DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		key := c.Param("key")
+		entry, err := db.GetEntry(key)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError,
+				"There is no entry behind given key.")
+		}
+
+		return c.JSON(http.StatusOK, entry)
+	}
+}
+
+func RedirectToShort(db *DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		url := c.Param("url")
+
+		goalURL, err := db.GetURL(url)
+		if err == keyNoValueErr {
+			return echo.NewHTTPError(http.StatusInternalServerError,
+				"There is no value behind given key.")
+		}
+
+		return c.Redirect(http.StatusTemporaryRedirect, "http://"+goalURL)
+	}
+}
+
+func main() {
+	// Echo instance
+	e := echo.New()
+
+	db := make(DB)
+
+	// Routes
+	e.GET("/api/hello", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{
+			"value": "Hello World!",
+		})
+	})
+	e.POST("/api/short", ShortURL(&db))
+	e.GET("/api/short/:key", GetShort(&db))
+	e.GET("/:url", RedirectToShort(&db))
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
